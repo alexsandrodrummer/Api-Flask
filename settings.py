b1c67265466f97017a83d72d64dcac6ca1707a29
Alex Sandro <79149058+alexsandrodummer@users.noreply.github.com>
2024-07-12 12:38:12 -0300

    Delete settings.py

--- settings.py
+++
@@ -1,8 +0,0 @@
-from app import app  # Importa o objeto app do módulo app
-from dynaconf import Dynaconf
-
-# Configurações usando Dynaconf
-settings = Dynaconf(
-    app,  # Objeto app do Flask
-    settings_files=["settings.toml", "secrets.toml"],  # Arquivos de configuração
-)
