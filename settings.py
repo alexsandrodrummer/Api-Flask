685040b0676a928435631516e4020c0d8c647bfa
Alex Sandro <79149058+alexsandrodummer@users.noreply.github.com>
2024-07-12 17:47:02 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,14 @@
-from app import app  # Importa o objeto app do módulo app
 from dynaconf import Dynaconf
 
-# Configurações usando Dynaconf
 settings = Dynaconf(
-    app,  # Objeto app do Flask
-    settings_files=["settings.toml", "secrets.toml"],  # Arquivos de configuração
+    envvar_prefix="DYNACONF",
+    settings_files=["settings.toml", "secrets.toml"],
 )
+
+class Config:
+    SECRET_KEY = settings.SECRET_KEY
+    SQLALCHEMY_DATABASE_URI = settings.DATABASE_URI
+    SQLALCHEMY_TRACK_MODIFICATIONS = settings.MODIFICATIONS
+
+class DevelopmentConfig(Config):
+    DEBUG = True
